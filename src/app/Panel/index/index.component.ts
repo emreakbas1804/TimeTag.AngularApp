4459e22b5d8465125fd6d46691a0c024732a4033
emre akbas <emreakbas042@gmail.com>
2025-07-05 02:26:47 +0300

    -

--- src/app/Panel/index/index.component.ts
+++ src/app/Panel/index/index.component.ts
@@ -1,36 +1,28 @@
 import { HttpClient } from '@angular/common/http';
-import { ChangeDetectorRef, Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatTableDataSource } from '@angular/material/table';
-import { firstValueFrom } from 'rxjs';
-import { EntityResultModel, Result } from 'src/app/Models/EntityResultModel';
-import { CompanyService } from 'src/app/Services/httpService/company.service';
+import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { EntityResultModel } from 'src/app/Models/EntityResultModel';
 
-declare var $: any;
 @Component({
     selector: 'app-index',
     templateUrl: './index.component.html',
     styleUrls: ['./index.component.css'],
-    encapsulation: ViewEncapsulation.None,
 })
 export class IndexComponent implements OnInit {
-    constructor(private http: HttpClient) {}
-
     dashboardResults: any;
 
-    ngOnInit(): void {
-        this.GetDashboardResults();
+    constructor(private http: HttpClient) {}
+
+    async ngOnInit(): Promise<void> {
+        await this.getDashboardResults();
     }
 
-    GetDashboardResults() {
-        this.http.get<EntityResultModel>(`${environment.apiUrl}/Common/GetDashboardResults`).subscribe({
-            next: response => {
-                this.dashboardResults = response.ResultObject;
-            },
-            error: err => {
-                console.error(err);
-            },
-        });
+    async getDashboardResults(): Promise<void> {
+        try {
+            const response = await this.http.get<EntityResultModel>(`${environment.apiUrl}/Common/GetDashboardResults`).toPromise();
+            this.dashboardResults = response?.ResultObject;
+        } catch (error) {
+            console.error('Error fetching dashboard results:', error);
+        }
     }
 }
