a43694299d7cb7702481d749ac6ebab2205f218f
emre akbas <emreakbas042@gmail.com>
2025-07-07 01:02:32 +0300

    -

--- src/app/Services/httpService/request-header.interceptor.ts
+++ src/app/Services/httpService/request-header.interceptor.ts
@@ -9,6 +9,7 @@ export class RequestHeaderInterceptor implements HttpInterceptor {
     constructor(private accountService: AccountService, private router: Router) {}
 
     intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+        debugger;
         const jwtToken = localStorage.getItem('accessToken');
         const langCode = localStorage.getItem('langCode') ?? 'en';
         if (jwtToken) {
@@ -16,6 +17,22 @@ export class RequestHeaderInterceptor implements HttpInterceptor {
                 setHeaders: {
                     Authorization: `Bearer ${jwtToken}`,
                     AccessToken: '1234567!',
+                    langCode: langCode,
+                },
+            });
+
+            return next.handle(clonedRequest).pipe(
+                catchError((error: HttpErrorResponse) => {
+                    if (error.status == 401 || error.status == 0) {
+                        // this.accountService.logOut();
+                        // this.router.navigate(['/panel']);
+                    }
+                    return throwError(() => error);
+                })
+            );
+        } else {
+            const clonedRequest = request.clone({
+                setHeaders: {
                     langCode: langCode,
                 },
             });
