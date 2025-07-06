2cdf26937372170a3dbbb58e493b061a4f78a27b
emre akbas <emreakbas042@gmail.com>
2025-07-06 23:48:02 +0300

    -

--- src/app/Home/login/login.component.ts
+++ src/app/Home/login/login.component.ts
@@ -8,65 +8,63 @@ import { SnackBarService } from 'src/app/Services/customService/snack-bar.servic
 import { AccountService } from 'src/app/Services/httpService/account.service';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor(private accountService: AccountService, private activatedRoute: ActivatedRoute, private route: Router, private translateService : TranslateService, private snackBarService : SnackBarService) { }
-  returnUrl = "/panel";
-  info: string | null = null;
-  infoColor: string | null = null;
-  loading: boolean = false;
-  ngOnInit(): void {
-    this.activatedRoute.queryParams.subscribe(c => {
-      this.returnUrl = c["returnUrl"] || "/panel";
-    })
-
-  }
-
-  login(form: NgForm) {
-    if (form.invalid) {      
-      this.info = this.translateService.instant("General.formValidationError");
-      this.infoColor = "danger";
-      return;
-    }
-    this.loading = true;
-    this.accountService.login(form.value.email, form.value.password).subscribe({
-      next: response => {
-        debugger
-        if (response.Result == Result.Success) {
-          this.route.navigate([this.returnUrl]);
-        }else if(response.Result == Result.Warning){
-          this.snackBarService.warning(response.ResultMessage);
-          this.route.navigate(["/forgot-password"])
-        } 
-        else {
-          this.info = response.ResultMessage;
-          this.infoColor = "danger";
-          this.loading = false;
-          window.scroll({
-            top: 0,
-            left: 0,
-            behavior: 'smooth'
-          });
-        }
-      },
-      error: err => {
-        this.info = err;
-        this.infoColor = "danger";
-        this.loading = false;
-        window.scroll({
-          top: 0,
-          left: 0,
-          behavior: 'smooth'
+    constructor(
+        private accountService: AccountService,
+        private activatedRoute: ActivatedRoute,
+        private route: Router,
+        private translateService: TranslateService,
+        private snackBarService: SnackBarService
+    ) {}
+    returnUrl = '/panel';
+    info: string | null = null;
+    infoColor: string | null = null;
+    loading: boolean = false;
+    ngOnInit(): void {
+        this.activatedRoute.queryParams.subscribe(c => {
+            this.returnUrl = c['returnUrl'] || '/panel';
         });
-      }
-    });
-  }
+    }
 
-
-
-
+    login(form: NgForm) {
+        if (form.invalid) {
+            this.info = this.translateService.instant('General.formValidationError');
+            this.infoColor = 'danger';
+            return;
+        }
+        this.loading = true;
+        this.accountService.login(form.value.email, form.value.password).subscribe({
+            next: response => {
+                debugger;
+                if (response.Result == Result.Success) {
+                    this.route.navigate([this.returnUrl]);
+                } else if (response.Result == Result.Warning) {
+                    this.snackBarService.warning(response.ResultMessage);
+                } else {
+                    this.info = response.ResultMessage;
+                    this.infoColor = 'danger';
+                    this.loading = false;
+                    window.scroll({
+                        top: 0,
+                        left: 0,
+                        behavior: 'smooth',
+                    });
+                }
+            },
+            error: err => {
+                this.info = err;
+                this.infoColor = 'danger';
+                this.loading = false;
+                window.scroll({
+                    top: 0,
+                    left: 0,
+                    behavior: 'smooth',
+                });
+            },
+        });
+    }
 }
