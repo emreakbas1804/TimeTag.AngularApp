f2cad1f7bfbd90d2ade0df9f61883afaef613963
emre akbas <emreakbas042@gmail.com>
2025-07-08 00:25:48 +0300

    -

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,23 +3,21 @@ import { AccountService } from './Services/httpService/account.service';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  title = 'TimeTag.AngularApp';
+    title = 'TimeTag.AngularApp';
 
-  constructor(private accountService: AccountService, public translateService: TranslateService) {
-    translateService.addLangs(["en", "tr"]);
-    var selectedLang = localStorage.getItem("langCode");
-    
-    
-    translateService.use(selectedLang ?? "en");
-  }
+    constructor(private accountService: AccountService, public translateService: TranslateService) {
+        translateService.addLangs(['en', 'tr', 'de']);
+        var selectedLang = localStorage.getItem('langCode');
 
+        translateService.use(selectedLang ?? 'en');
+    }
 
-  ngOnInit(): void {
-    this.accountService.autoLogin();
-  }
+    ngOnInit(): void {
+        this.accountService.autoLogin();
+    }
 }
